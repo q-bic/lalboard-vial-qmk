71927d177d3ea978f04274bd83676673e373669d
q-bic <qbiccl@gmail.com>
2026-03-30 14:55:51 +0200

    remove "set_auto_mouse_layer" from "keymap.c" as this is already defined in "config.h"

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -39,7 +39,7 @@ void keyboard_post_init_user(void) {
 }
 
 void pointing_device_init_user(void) {
-    set_auto_mouse_layer(5); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
+    //set_auto_mouse_layer(5); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
     set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
 }
 
